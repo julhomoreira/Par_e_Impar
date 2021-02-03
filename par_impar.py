b0d8fb183ee74695a289e12f8922beed3ad1a000
Julho César Moreira <julhomoreira1988@gmail.com>
2021-02-03 11:28:52 -0300

    código do progarma

--- par_impar.py
+++ par_impar.py
@@ -1,2 +1,19 @@
 
 
+nome = input('Digite seu nome: ')
+usua1 = input('Agora Digite um número inteiro: ')
+
+if usua1.isdigit():
+    usua1 = int(usua1)
+
+    if usua1 % 2 == 0:
+        print('='*10)
+        print(f'{nome} digitou o número {usua1} e é PAR.')
+    else:
+        print('='*10)
+        print(f'{nome} digitu o número {usua1} e é ÍMPAR.')
+
+else:
+    print('-*-' * 20)
+    print('Entada incorreta! Por favor, digite somente números inteiros!')
+
