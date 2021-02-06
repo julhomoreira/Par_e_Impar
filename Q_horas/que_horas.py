b8219199869be4e2dba3c8a4cff170eeffc8adcb
Julho César Moreira <julhomoreira1988@gmail.com>
2021-02-06 15:07:32 -0300

    perguntando o horáio

---
+++ Q_horas/que_horas.py
@@ -0,0 +1,15 @@
+nome = input('Qual é o seu nome? ')
+usuario = float(input(f'{nome}, Que horas são? '))
+
+if usuario >= 0 and usuario <= 11:
+    print(f'Bom dia!')
+
+elif usuario >= 12 and usuario <= 17:
+    print('Boa tarde!')
+
+elif usuario >= 18 and usuario <= 23:
+    print('Boa noite')
+
+else:
+    print('Boa madrugada! ')
+
